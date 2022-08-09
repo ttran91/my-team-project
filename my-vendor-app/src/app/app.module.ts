e3db6b7815bd82e66a625caeeb283d860cd61624
Umair Ali <umairmamoor@gmail.com>
2022-08-08 21:39:54 -0400

    basic structure

--- my-vendor-app/src/app/app.module.ts
+++ my-vendor-app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { AuthComponent } from './auth/auth.component';
+import { ModelComponent } from './model/model.component';
+import { ServiceComponent } from './service/service.component';
+import { VendorComponent } from './components/vendor/vendor.component';
+import { OrderComponent } from './components/order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    AuthComponent,
+    ModelComponent,
+    ServiceComponent,
+    VendorComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
