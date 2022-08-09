62c8ba585cc482500992db58c718316143bbc9a5
Umair Ali <umairmamoor@gmail.com>
2022-08-09 13:51:07 -0400

    xyz

--- my-vendor-app/src/app/app.module.ts
+++ my-vendor-app/src/app/app.module.ts
@@ -6,10 +6,10 @@ import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { ModelComponent } from './model/model.component';
 import { VendorComponent } from './components/vendor/vendor.component';
-import { OrderComponent } from './components/order/order/';
-import { OrderAddComponent } from './components/order/order-add/order-add';
-import { OrderListComponent } from './components/order/order-list/order-list';
-import { OrderStatComponent } from './components/order/order-stat/order-stat';
+import { OrderComponent } from './components/order/order/order.component';
+import { OrderAddComponent } from './components/order/order-add/order-add.component';
+import { OrderListComponent } from './components/order/order-list/order-list.component';
+import { OrderStatComponent } from './components/order/order-stat/order-stat.component';
 
 
 @NgModule({
