b9a3ec0ac0930a8612dc461cc82353aaa3c19970
Umair Ali <umairmamoor@gmail.com>
2022-08-09 21:43:14 -0400

    CrosConfig file and OrderForm GET method with pagenation

--- MyBackEndProject/src/main/java/com/springboot/backend/controller/OrderFormController.java
+++ MyBackEndProject/src/main/java/com/springboot/backend/controller/OrderFormController.java
@@ -1,8 +1,15 @@
 package com.springboot.backend.controller;
 
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.backend.model.OrderForm;
@@ -24,6 +31,17 @@ public class OrderFormController {
 		
 	}
 	
+	
+	@GetMapping("/orderForm")
+	public List<OrderForm> getAllOrderFormData(
+					@RequestParam("page") Integer page,
+					@RequestParam("size") Integer size){
+			
+		Pageable pageable=PageRequest.of(page, size);
+		
+		return orderFormRepository.findAll(pageable).getContent();
+		
+	}
 	
 	
 	
