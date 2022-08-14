ee8090ba5101c1b1de175d9c1a24b553cd5d7e21
Umair Ali <umairmamoor@gmail.com>
2022-08-14 02:04:01 -0400

    edit function in list

--- my-vendor-app/src/app/components/order/order-edit/order-edit.component.ts
+++ my-vendor-app/src/app/components/order/order-edit/order-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { OrderFormEditDto } from '../model/OrderFormEdit';
+import { OrderService } from '../service/order.service';
 
 @Component({
   selector: 'app-order-edit',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderEditComponent implements OnInit {
 
-  constructor() { }
+  info: string;
+  orderFormEditDto: OrderFormEditDto;
+  orderEditForm: FormGroup;
+  msg: string;
+
+
+  constructor(private orderService: OrderService) { }
 
   ngOnInit(): void {
+    this.msg='';
+    this.orderEditForm = new FormGroup({
+      cName: new FormControl(''),
+      orderStatus: new FormControl(''),
+      orderCost: new FormControl(''),
+      cPnumber: new FormControl('')
+    });
+}
+
+  onFormSubmit(){
+    this.orderFormEditDto={
+      cName: this.orderEditForm.value.cName,
+      orderStatus: this.orderEditForm.value.orderStatus,
+      orderCost: this.orderEditForm.value.orderCost,
+      cPnumber: this.orderEditForm.value.cPnumber
+
+
+    };
+    this.orderService.editForm(this.info).subscribe({
+      next: (data)=> {
+        this.msg="Order Updated!!!";
+        this.orderEditForm.controls['cName'].setValue(this.orderFormEditDto.cName);
+        this.orderEditForm.controls['orderStatus'].setValue(this.orderFormEditDto.orderStatus);
+        this.orderEditForm.controls['orderCost'].setValue(this.orderFormEditDto.orderCost);
+        this.orderEditForm.controls['cPnumber'].setValue(this.orderFormEditDto.cPnumber);
+        
+
+
+      },
+      error: (e)=> {
+        this.msg="OrderEdit Operation Failed";
+      }
+    });
   }
 
 }
--- my-vendor-app/src/app/components/order/service/order.service.ts
+++ my-vendor-app/src/app/components/order/service/order.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { OrderForm } from '../model/order.model';
+import { OrderFormEditDto } from '../model/OrderFormEdit';
 import { Stat } from '../model/Stat.model';
 
 @Injectable({
@@ -20,6 +21,7 @@ export class OrderService {
   orderStat$ = new BehaviorSubject<any>(0);
   page$ = new BehaviorSubject<number>(0);
   stat$ = new BehaviorSubject<Boolean>(false);
+  putformEditApi: string;
 
 
 
@@ -28,6 +30,7 @@ export class OrderService {
     this.getAllApi=environment.serverUrl+'/orderForm';
     this.deleteApi=environment.serverUrl+'/orderForm/';
     this.getStatsApi=environment.serverUrl+'/orderForm/stats';
+    this.putformEditApi=environment.serverUrl+'/orderForm/edit/';
 
 
   
@@ -49,6 +52,17 @@ export class OrderService {
    getOrderFormStats(): Observable<Stat[]> {
     return this.http.get<Stat[]>(this.getStatsApi);
 
+   }
+
+   editForm(info: string) :Observable<OrderFormEditDto>{
+    let httpOptions={
+      headers : new HttpHeaders({
+        'Content-type' : 'application/json',
+        'Authorization' : 'basic' + info
+
+      })
+    };
+    return this.http.put<OrderFormEditDto>(this.putformEditApi,OrderFormEditDto,httpOptions);
    }
 
 
